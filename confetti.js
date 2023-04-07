31d2bffda64d3a09ab91efaa525f51725f847c3e
tbdiah <110161828+tbdiah@users.noreply.github.com>
2023-04-07 13:55:56 -0400

    udpated to stop confetti

--- confetti.js
+++ confetti.js
@@ -7,29 +7,21 @@ function launchConfetti() {
   canvas.width = width;
   canvas.height = height;
 
-  function frame() {
-    context.clearRect(0, 0, width, height);
-const confettiSettings = {
-      particleCount: 3,
-      angle: Math.random() * 360,
-      spread: Math.random() * 90,
-      origin: {
-        x: Math.random(),
-        y: Math.random() - 0.2
-      },
-      colors: [
-        [165, 104, 246],
-        [230, 61, 135],
-        [0, 199, 228],
-        [253, 214, 126]
-      ]
-    };
+  const confettiSettings = {
+    particleCount: 200,
+    spread: 70,
+    origin: {
+      y: 0.6
+    },
+    colors: [
+      [165, 104, 246],
+      [230, 61, 135],
+      [0, 199, 228],
+      [253, 214, 126]
+    ]
+  };
 
-    confetti(confettiSettings);
-    requestAnimationFrame(frame);
-  }
-
-  frame();
+  confetti(confettiSettings);
 }
 
 launchConfetti();
