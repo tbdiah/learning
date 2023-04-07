125946184731cf677f34594333f792daaaf02736
tbdiah <110161828+tbdiah@users.noreply.github.com>
2023-04-07 13:24:20 -0400

    new script

--- confetti.js
+++ confetti.js
@@ -1,24 +1,35 @@
-// JavaScript Document
 function launchConfetti() {
-  const confettiSettings = {
-    target: 'confetti-canvas',
-    max: 200,
-    size: 1,
-    animate: true,
-    props: ['circle', 'square', 'triangle', 'line'],
-    colors: [
-      [165, 104, 246],
-      [230, 61, 135],
-      [0, 199, 228],
-      [253, 214, 126]
-    ],
-    clock: 25,
-    interval: 1000 / 60,
-    zIndex: 10000
-  };
+  const canvas = document.getElementById('confetti-canvas');
+  const context = canvas.getContext('2d');
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-  const confetti = new ConfettiGenerator(confettiSettings);
-  confetti.render();
+  canvas.width = width;
+  canvas.height = height;
+
+  function frame() {
+    context.clearRect(0, 0, width, height);
+    const confettiSettings = {
+      particleCount: 1,
+      angle: Math.random() * 360,
+      spread: Math.random() * 90,
+      origin: {
+        x: Math.random(),
+        y: Math.random() - 0.2
+      },
+      colors: [
+        [165, 104, 246],
+        [230, 61, 135],
+        [0, 199, 228],
+        [253, 214, 126]
+      ]
+    };
+
+    confetti(confettiSettings);
+    requestAnimationFrame(frame);
+  }
+
+  frame();
 }
 
 launchConfetti();
