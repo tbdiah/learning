b1cdd9a9995bcae8474b7d952e7a6afcb991da20
tbdiah <110161828+tbdiah@users.noreply.github.com>
2023-04-07 12:33:29 -0400

    adding confetti

---
+++ confetti.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function launchConfetti() {
+  const canvas = document.getElementById('confetti-canvas');
+  const confettiSettings = {
+    target: canvas,
+    max: 200,
+    size: 1,
+    animate: true,
+    props: ['circle', 'square', 'triangle', 'line'],
+    colors: [
+      [165, 104, 246],
+      [230, 61, 135],
+      [0, 199, 228],
+      [253, 214, 126]
+    ],
+    clock: 25,
+    interval: 1000 / 60,
+    zIndex: 10000
+  };
+
+  const confetti = new ConfettiGenerator(confettiSettings);
+  confetti.render();
+}
+
+launchConfetti();
